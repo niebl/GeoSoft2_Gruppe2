c399fc28196e9504d2c4c00352d3dfd66432d476
dhenn12 <53432717+dhenn12@users.noreply.github.com>
2019-10-31 16:05:39 +0100

    -update1

---
+++ Scripts/leaflet.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([0, 0], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 13
+}).addTo(map);
+
+
+L.marker([0,0])
+  .bindPopup('Null Island!')
+  .addTo(map);
--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 
+
 //setting view engine to pug
 app.set('view engine', 'pug');
+
+// files from packages needed
+app.use("/leaflet", express.static(__dirname + "/node_modules/leaflet/dist"));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+// use scripts, styles in webserver
+app.use("/stylesheetpug", express.static(__dirname + '/style/stylesheetpug.css'));
+app.use("/leafletscript", express.static(__dirname + '/Scripts/leaflet.js'));
 
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get('/main', function(req, res) {
+  res.render('initmap');
+});
+
+
+
 
 
 app.listen(3000, () => console.log("app is listening the silence"));
