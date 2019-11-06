a39fb7de0dd5b96480615de674d7e8173ef70d4d
dhenn12 <53432717+dhenn12@users.noreply.github.com>
2019-11-06 14:46:05 +0100

    server

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//setting view engine to pug
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
-  res.render('index');
+// mongoose setup
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+// twitter data in mongoose
+var twitter = mongoose.model('Tweet', {url: String, location: String, author: String, date: String, text: String});
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(3000, () => console.log("app is listening the silence"));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
