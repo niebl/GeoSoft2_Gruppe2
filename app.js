1d54cece1e8aaf6f0763c8a7e45b071d29128f2e
dhenn12 <53432717+dhenn12@users.noreply.github.com>
2019-11-07 15:18:51 +0100

    s

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+app.get('/', function(req, res) {
+  res.render('index');});
+
 // mongoose setup
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // twitter data in mongoose
 var twitter = mongoose.model('Tweet', {url: String, location: String, author: String, date: String, text: String});
@@ -27,8 +31,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use("/leaflet", express.static(__dirname + "/node_modules/leaflet/dist"));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+// use scripts, styles in webserver
+app.use("/stylesheetpug", express.static(__dirname + '/public/stylesheets/style.css'));
+app.use("/leafletscript", express.static(__dirname + '/public/javascripts/leaflet.js'));
+
+
+app.get('/getdefaultlocation', function(req, res) {
+    var location = req.cookies.coords;
+    res.send(location);
+});
+app.get("/", (req, res)=>{res.send("hello world")});
+app.get('/setdefaultlocation1/:lat/:lng', function(req, res){
+  //res.clearCookie("coords");
+  var position = [];
+  postion.push(req.params.lat);
+  postion.push(req.params.lng);
+  res.cookie('coords', postion, {httpOnly: true, secure: true, path:'/'});
+  res.send(req.params.lat);
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
