9c37e9cfc01272082c19087aa8e0e95ad9988024
dhenn12 <53432717+dhenn12@users.noreply.github.com>
2019-11-07 15:47:52 +0100

    Adding Map Layers

--- public/javascripts/leaflet.js
+++ public/javascripts/leaflet.js
@@ -1,11 +1,31 @@
-var map = L.map('map').setView([0, 0], 4);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    id: 'map',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 13
-}).addTo(map);
+});
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  id: 'map',
+  maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  id: 'map',
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var baseMaps = {
+	"Topographic": topo,
+	"Streets": street,
+  "Satellite": satellite
+};
+
+var map = L.map('map', {
+  layers:  [topo]
+}).setView([0, 0], 4);
 
 
-L.marker([0,0])
-  .bindPopup('Null Island!')
-  .addTo(map);
+L.control.layers(baseMaps).addTo(map);
