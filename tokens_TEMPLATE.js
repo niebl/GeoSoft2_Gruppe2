aca4a01da9578e6886bc5f4f330af2d69c42f90d
Dingensen <f_nieb02@uni-muenster.de>
2019-11-27 11:07:58 +0100

    add package twitter. https://www.npmjs.com/package/twitter

--- tokens_TEMPLATE.js
+++ tokens_TEMPLATE.js
@@ -1,13 +1,15 @@
+/*jshint esversion: 6 */
+
 //API KEYS
 //TEMPLATE FILE
 // copy this file, name it "tokens.js" and insert the given keys into the given parameters.
+// double check that "tokens.js" is in .gitignore
 // DO NOT PUBLISH ANY TOKENS ON GITHUB
 // if you do, please notify us before resetting the tokens.
 
-module.export = {
-  //Twitter
-  consumer_key = "KEY HERE"
-  consumer_secret = "KEY HERE"
-  access_token = "KEY HERE"
-  access_secret = "KEY HERE"
-}
+module.exports = {
+  consumer_key: "KEY HERE",
+  consumer_secret: "KEY HERE",
+  access_token: "KEY HERE",
+  access_secret: "KEY HERE"
+};
