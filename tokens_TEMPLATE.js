c97e363783c94451fd3740d94b0745b6bf609a6c
Dingensen <f_nieb02@uni-muenster.de>
2019-11-27 11:32:49 +0100

    add tweet query in twitterApiExt.js

--- tokens_TEMPLATE.js
+++ tokens_TEMPLATE.js
@@ -8,8 +8,9 @@
 // if you do, please notify us before resetting the tokens.
 
 module.exports = {
-  consumer_key: "KEY HERE",
-  consumer_secret: "KEY HERE",
-  access_token: "KEY HERE",
-  access_secret: "KEY HERE"
+  twitter: {
+    consumer_key: "KEY HERE",
+    consumer_secret: "KEY HERE",
+    access_token_key: "KEY HERE",
+    access_token_secret: "KEY HERE"}
 };
--- twitterApiExt.js
+++ twitterApiExt.js
@@ -1,2 +1,19 @@
 /*jshint esversion: 6 */
 const token = require('./tokens.js');
+
+//use twitter package. src: https://github.com/jdub/node-twitter
+var Twitter = require("twitter")
+
+//create a client object for the external twitter api
+//the given api keys are a parameter
+var client = new Twitter(
+  token.twitter
+);
+
+//just a test
+var params = {screen_name: 'DWD_presse'}
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  } else {console.log(error)}
+});
