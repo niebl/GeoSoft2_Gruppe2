d6956d3c5247558b34f4d0d8357ed8837f74281c
ReneStalitza <Renestalitza@web.de>
2020-01-26 16:11:27 +0100

    new

---
+++ tokens_TEMPLATE.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+//API KEYS
+//TEMPLATE FILE
+// copy this file, name it "tokens.js" and insert the given keys into the given parameters.
+// double check that "tokens.js" is in .gitignore
+// DO NOT PUBLISH ANY TOKENS ON GITHUB
+// if you do, please notify us before resetting the tokens.
+
+module.exports = {
+  twitter: {
+    consumer_key: "KEY HERE",
+    consumer_secret: "KEY HERE",
+    access_token_key: "KEY HERE",
+    access_token_secret: "KEY HERE"}
+};
